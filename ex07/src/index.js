32dcbd40e7d6024a4e064f2a470d121dbe685bdd
dino-ha <hadzirovicdino@gmail.com>
2022-04-01 16:08:23 +0200

    Add files via upload

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,1 +1,10 @@
+function main(){
 
+        var myName = "George";
+        var myString = "My name is " + myName + " and I am well!";
+        return myString;
+
+}
+
+console.log(main());
+module.exports = main;
