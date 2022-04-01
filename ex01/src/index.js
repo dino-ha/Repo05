7726645a2c5001a38dfc147216fd0ff4deac516b
dino-ha <hadzirovicdino@gmail.com>
2022-04-01 16:03:06 +0200

    Add files via upload

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,17 @@
+function main(){
 
+	var myFirstName = "Dino";
+	var myLastName = "Hadzirovic";
+	var myHomeTown = "Sarajevo";
+
+	return {
+		myFirstName,
+		myLastName,
+		myHomeTown
+
+};
+}
+
+console.log(main());
+
+module.exports = main;
